1bdcf2a48f66286731efbcbd4fee21188c5274c6
William Oldham <wegg7250@gmail.com>
2016-08-26 15:35:07 +0100

    Combined nested if statments

--- src/es/esy/williamoldham/security/listeners/SecurityLoginListener.java
+++ src/es/esy/williamoldham/security/listeners/SecurityLoginListener.java
@@ -12,28 +12,26 @@ public class SecurityLoginListener implements Listener{
 
 	private boolean needsUpdate;
 	private Update update;
-	
+
 	public SecurityLoginListener(boolean needsUpdate, Update update) {
 		this.needsUpdate = needsUpdate;
 		this.update = update;
 	}
-	
-	
+
+
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent e){
-		
+
 		Player player = e.getPlayer();
-		
-		if (player.hasPermission("security.updatenotify") || player.hasPermission("security.*") || PlayerUtilities.hasOP(player)) {
-			if(needsUpdate){
-				player.sendMessage("[Security Updater] Found new version: " + update.getNewVer() + "! (Your version is " + update.getOldVer() + ")");
-		        player.sendMessage("[Security Updater] Download here: " + update.getURL());
-			}
+
+		if ((player.hasPermission("security.updatenotify") || player.hasPermission("security.*") || PlayerUtilities.hasOP(player)) && needsUpdate) {
+			player.sendMessage("[Security Updater] Found new version: " + update.getNewVer() + "! (Your version is " + update.getOldVer() + ")");
+			player.sendMessage("[Security Updater] Download here: " + update.getURL());
 		}
-		
+
 	}
-	
-	
+
+
 }
 
 
